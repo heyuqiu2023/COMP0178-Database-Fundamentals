8d09f462d3fd3da22a0347e8e2505d53a7317c12
li8k6z8 <duktig2233@gmail.com>
2025-11-30 15:46:21 +0000

    Delete auction_system directory

--- auction_system/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirect the home page to the browse page.  This keeps index.php simple
-// and allows browse.php to serve as the primary landing page of the site.
-header('Location: /auction_system/browse.php');
-exit();
